93178ea62ef86dabca00559cce45a33f95b9b0c2
Vijayhrithikk <vijaymonish11@gmail.com>
2026-05-28 22:25:53 +0530

    move redirects to /r route

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -18,7 +18,7 @@ func SetupRoutes(router *gin.Engine, cfg *config.Config) {
 		authRoutes.POST("/login", authRouter.Login)
 	}
 	router.POST("/shorten", middleware.AuthMiddleware(cfg), handlers.CreateShortURL)
-	router.GET("/:code", handlers.RedirectURL)
+	router.GET("/r/:code", handlers.RedirectURL)
 	router.GET("/my-urls", middleware.AuthMiddleware(cfg), handlers.GetMyURLs)
 	router.DELETE("/urls/:id", middleware.AuthMiddleware(cfg), handlers.DeleteURL)
 }
